eb3edda8370975c5872855f8d8920c7f89eeffdb
Rui Oliveira <ruimail24@gmail.com>
2021-11-09 19:17:33 +0000

    Basic stuff for bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +16,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
