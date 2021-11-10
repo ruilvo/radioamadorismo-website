f0e09aa80803e7a17d70a6e2b6b6eb90836a07e3
Rui Oliveira <ruimail24@gmail.com>
2021-11-10 13:44:42 +0000

    Create a basic page for the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,53 @@
-import { Helmet } from "react-helmet-async";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <h1 className="site-heading">
-      <Helmet>
-        <title>Hello World</title>
-      </Helmet>
-      Hello world!
-    </h1>
+    <div>
+      <Header />
+      <Container fluid="xl">
+        <h1>Hello World</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus
+        tincidunt sollicitudin. Donec ut justo sed turpis euismod fermentum non
+        ullamcorper massa. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. In viverra lacus metus, ac convallis tellus varius vitae.
+        Aenean feugiat tellus a est tempus, id viverra ligula volutpat. Duis nec
+        elit vel neque cursus tincidunt. Maecenas condimentum ante a massa
+        viverra, sit amet fringilla odio lacinia. Sed mollis tortor nec ipsum
+        vulputate sagittis. Nunc et diam euismod, tempus sapien hendrerit,
+        suscipit leo. Quisque finibus metus at pulvinar pretium. Integer euismod
+        nisi nec sapien imperdiet, vitae auctor diam placerat. Aliquam cursus,
+        mi non faucibus vehicula, augue purus laoreet ex, sed convallis lorem ex
+        sit amet eros. Nam in purus finibus, condimentum sem vel, hendrerit
+        purus. In ac orci erat. Morbi in ex id velit faucibus fermentum.
+        Curabitur eget nibh efficitur, efficitur lorem sed, varius arcu. Quisque
+        ornare erat libero, et fringilla nulla iaculis quis. In hac habitasse
+        platea dictumst. Pellentesque ac tellus risus. Quisque tortor ipsum,
+        maximus eu tellus ut, mollis placerat sem. Phasellus finibus ligula id
+        iaculis tincidunt. Maecenas eu lectus ac arcu porta sodales. Fusce dolor
+        ipsum, gravida nec fermentum a, tristique eget justo. Morbi lorem elit,
+        tincidunt eget scelerisque ut, semper et sapien. Donec non sollicitudin
+        quam, eu egestas dui. Vivamus eget varius mi. Nunc id sem nisi. Integer
+        sodales, nibh a facilisis placerat, eros enim tincidunt dui, sed maximus
+        nibh mi a metus. Fusce sagittis urna id neque tincidunt, id rhoncus
+        lorem molestie. Sed ac consectetur ante. Nunc finibus ligula ipsum,
+        vitae vehicula nunc suscipit nec. Duis accumsan lacinia risus eget
+        congue. Vestibulum at posuere purus. Praesent varius augue et mauris
+        auctor suscipit. Maecenas imperdiet lacus tellus, malesuada interdum
+        lacus condimentum non. Ut ac pretium lacus, et venenatis diam. Praesent
+        vitae porttitor lorem. Duis volutpat risus fermentum ex rhoncus auctor.
+        In pretium turpis a efficitur facilisis. Fusce auctor dapibus feugiat.
+        Curabitur hendrerit laoreet mauris eu rutrum. Etiam nec pharetra nunc,
+        id commodo ipsum. Integer eu rutrum metus. Duis vel ornare mauris.
+        Integer ac lacus dapibus augue interdum molestie eget eu ipsum. Donec ac
+        consequat ex, non porta velit. Integer finibus ex at metus posuere
+        elementum. Quisque erat ligula, tempus nec porttitor eu, condimentum non
+        odio. Vivamus lacinia, justo sed venenatis ullamcorper, dui enim lacinia
+        erat, eget bibendum odio dolor id ligula. Curabitur tempor enim eget
+        arcu blandit, ac suscipit metus bibendum. Nunc ut leo ultricies, egestas
+        ante ac, condimentum lorem.
+      </Container>
+    </div>
   );
 }
 
