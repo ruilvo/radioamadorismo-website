16557917f8640001c74a25de32bd7eb21b70b384
Rui Oliveira <ruimail24@gmail.com>
2021-11-19 19:11:02 +0000

    Add leaflet

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,6 @@ import router from "./router";
 import titleMixin from "./mixins/titleMixin";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import "leaflet/dist/leaflet.css";
 
 createApp(App).mixin(titleMixin).use(router).mount("#app");
--- frontend/src/views/Repeaters.vue
+++ frontend/src/views/Repeaters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container" style="max-width: 1000px">
     <h2>Base de dados dos repetidores</h2>
 
     <table class="table thead-light table-striped">
@@ -18,11 +18,21 @@
         </tr>
       </tbody>
     </table>
+    <div id="map"></div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+#map {
+  width: 600px;
+  height: 400px;
+  margin: auto;
+}
+</style>
+
 <script>
 import axios from "axios";
+import * as L from "leaflet";
 
 export default {
   name: "Repeaters",
@@ -34,6 +44,20 @@ export default {
 
   mounted() {
     this.getRepeaters();
+    var mymap = L.map("map").setView([40, -8.0], 6);
+
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+      {
+        maxZoom: 18,
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+      }
+    ).addTo(mymap);
   },
 
   methods: {
