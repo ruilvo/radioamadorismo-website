808fd3a14f7bee90bee072f232f1ebdfde90c523
Rui Oliveira <ruimail24@gmail.com>
2021-11-20 20:12:55 +0000

    Basic layouting

--- frontend/src/views/Repeaters.vue
+++ frontend/src/views/Repeaters.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="container" style="max-width: 1000px">
-    <h2>Base de dados dos repetidores</h2>
+  <h2 class="text-center">Base de dados dos repetidores</h2>
 
-    <table class="table thead-light table-striped">
-      <thead class="table-dark">
-        <tr>
-          <th>Indicativo</th>
-          <th>Região</th>
-          <th>Local</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in repeaters" :key="item.callsign">
-          <th>{{ item.callsign }}</th>
-          <th>{{ item.info_location.region }}</th>
-          <th>{{ item.info_location.place }}</th>
-        </tr>
-      </tbody>
-    </table>
-    <Map />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="text-center">
+          <h4>Filtros</h4>
+        </div>
+        Em construção...
+      </div>
+      <div class="col-sm-9">
+        <table class="table thead-light table-striped">
+          <thead class="table-dark">
+            <tr>
+              <th>Indicativo</th>
+              <th>Região</th>
+              <th>Local</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in repeaters" :key="item.callsign">
+              <th>{{ item.callsign }}</th>
+              <th>{{ item.info_location.region }}</th>
+              <th>{{ item.info_location.place }}</th>
+            </tr>
+          </tbody>
+        </table>
+        <Map />
+      </div>
+    </div>
   </div>
 </template>
 
