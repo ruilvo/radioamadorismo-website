d794d9bfa557d44ed71da5b3a4da1fc95ed302a9
Rui Oliveira <ruimail24@gmail.com>
2021-11-20 09:42:45 +0000

    Separate the Map into a component

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<style lang="scss" scoped>
+#map {
+  max-width: 600px;
+  height: 400px;
+  margin: auto;
+}
+</style>
+
+<script>
+import * as L from "leaflet";
+
+export default {
+  name: "Repeaters",
+  props: {
+    markers: [],
+  },
+
+  mounted() {
+    var mymap = L.map("map").setView([40, -8.0], 6);
+
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+      {
+        maxZoom: 18,
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+      }
+    ).addTo(mymap);
+  },
+};
+</script>
--- frontend/src/views/Repeaters.vue
+++ frontend/src/views/Repeaters.vue
@@ -18,21 +18,13 @@
         </tr>
       </tbody>
     </table>
-    <div id="map"></div>
+    <Map />
   </div>
 </template>
 
-<style lang="scss" scoped>
-#map {
-  max-width: 600px;
-  height: 400px;
-  margin: auto;
-}
-</style>
-
 <script>
 import axios from "axios";
-import * as L from "leaflet";
+import Map from "../components/Map.vue";
 
 export default {
   name: "Repeaters",
@@ -42,22 +34,12 @@ export default {
     };
   },
 
+  components: {
+    Map,
+  },
+
   mounted() {
     this.getRepeaters();
-    var mymap = L.map("map").setView([40, -8.0], 6);
-
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-      {
-        maxZoom: 18,
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1,
-      }
-    ).addTo(mymap);
   },
 
   methods: {
