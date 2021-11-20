53426aa61184c39d0943e1f14c0715bec58be04d
Rui Oliveira <ruimail24@gmail.com>
2021-11-20 14:28:33 +0000

    Fix wrong basename

--- backend/repeaters/urls.py
+++ backend/repeaters/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import SimpleRouter
 from .views import FactRepeaterViewSet
 
 router = SimpleRouter()
-router.register("", FactRepeaterViewSet, basename="posts")
+router.register("", FactRepeaterViewSet, basename="repeaters")
 
 urlpatterns = router.urls
