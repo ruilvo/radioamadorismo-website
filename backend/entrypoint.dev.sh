18d7c657cc9fbcd38d55ea611925a3cbb3bd908b
Rui Oliveira <ruimail24@gmail.com>
2021-11-24 22:08:04 +0000

    Have the processes end gracefully

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
-python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8000
--- frontend/entrypoint.dev.sh
+++ frontend/entrypoint.dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run serve
+exec npm run serve
