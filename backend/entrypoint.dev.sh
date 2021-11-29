e8831d402685643cb407d81f0edd6701fb5d7ad6
Rui Oliveira <ruimail24@gmail.com>
2021-11-29 22:43:23 +0000

    Wait for pgsql

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "Waiting for postgres..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
 python manage.py migrate --noinput
 exec python manage.py runserver 0.0.0.0:8000
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Waiting for postgres..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
 exec gunicorn config.wsgi -b 0.0.0.0:8000
