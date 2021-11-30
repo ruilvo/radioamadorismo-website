f2c5280e0daa417aba566c1e82b33285fb2d1c26
Rui Oliveira <ruimail24@gmail.com>
2021-11-30 20:42:34 +0000

    Use v-model instead

--- frontend/src/components/repeaters/Filters.vue
+++ frontend/src/components/repeaters/Filters.vue
@@ -4,13 +4,7 @@
   </div>
 
   <label for="mode-filter">Modo</label>
-  <select
-    class="form-select"
-    multiple
-    @change="handleModeFilter"
-    id="mode-filter"
-    ref="mode-filter"
-  >
+  <select class="form-select" multiple v-model="selectedModes">
     <option value="fm">FM</option>
     <option value="dstar">D-STAR</option>
     <option value="fusion">Fusion/C4FM</option>
@@ -23,17 +17,14 @@ import state from "../../shared/repeaters.js";
 
 export default {
   name: "Filters",
+  data() {
+    return {
+      selectedModes: [],
+    };
+  },
 
   mounted() {
     state.getRepeaters();
-  },
-
-  methods: {
-    handleModeFilter() {
-      const selectedOptions = [...this.$refs["mode-filter"].selectedOptions];
-      const selectedValues = selectedOptions.map((option) => option.value);
-      console.log(selectedValues);
-    },
   },
 };
 </script>
