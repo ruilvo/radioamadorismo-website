0ba8640efa8b8341dc835cfc14a2a17e0e91fa51
Rui Oliveira <ruimail24@gmail.com>
2021-11-30 20:26:49 +0000

    Add basic options handling

--- frontend/src/components/repeaters/Filters.vue
+++ frontend/src/components/repeaters/Filters.vue
@@ -2,14 +2,38 @@
   <div class="text-center">
     <h4>Filtros</h4>
   </div>
-  Em construção...
+
+  <label for="mode-filter">Modo</label>
+  <select
+    class="form-select"
+    multiple
+    @change="handleModeFilter"
+    id="mode-filter"
+    ref="mode-filter"
+  >
+    <option value="fm">FM</option>
+    <option value="dstar">D-STAR</option>
+    <option value="fusion">Fusion/C4FM</option>
+    <option value="dmr">DMR</option>
+  </select>
 </template>
 
 <script>
+import state from "../../shared/repeaters.js";
+
 export default {
   name: "Filters",
-  props: {
-    repeaters: Array,
+
+  mounted() {
+    state.getRepeaters();
+  },
+
+  methods: {
+    handleModeFilter() {
+      const selectedOptions = [...this.$refs["mode-filter"].selectedOptions];
+      const selectedValues = selectedOptions.map((option) => option.value);
+      console.log(selectedValues);
+    },
   },
 };
 </script>
