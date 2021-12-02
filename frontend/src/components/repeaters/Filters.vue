ebba78473cd9d9d509af789c2f0ae0f8f6277017
Rui Oliveira <ruimail24@gmail.com>
2021-12-02 10:28:02 +0000

    Add filtering form

--- frontend/src/components/repeaters/Filters.vue
+++ frontend/src/components/repeaters/Filters.vue
@@ -3,50 +3,58 @@
     <h4>Filtros</h4>
   </div>
 
-  <label for="region-filter">Região</label>
-  <select
-    class="form-select"
-    multiple
-    v-model="selectedRegions"
-    id="region-filter"
-  >
-    <option value="CPT">Continente</option>
-    <option value="AZR">Açores</option>
-    <option value="MDA">Madeira</option>
-    <option value="OT">Outros</option>
-  </select>
-
-  <label for="mode-filter">Modo</label>
-  <select class="form-select" multiple v-model="selectedModes" id="mode-filter">
-    <option value="fm">FM</option>
-    <option value="dstar">D-STAR</option>
-    <option value="fusion">Fusion/C4FM</option>
-    <option value="dmr">DMR</option>
-  </select>
-
-  <label for="rf-filter">RF</label>
-  <select class="form-select" multiple v-model="selectedRf" id="rf-filter">
-    <option value="simplex">Simplex</option>
-    <option value="half_duplex">Semi-duplex</option>
-  </select>
-
-  <label for="modulation-filter" class="form-label">Modulação</label>
-  <input
-    type="email"
-    class="form-control"
-    id="modulation-filter"
-    placeholder="Ex.: 11kHzF3E"
-    v-model="modulation"
-  />
-
-  <label for="freq-filter" class="form-label">Frequência</label>
-  <div class="input-group" id="freq-filter">
-    <input type="number" class="form-control" />
-    <div class="input-group-prepend">
-      <span class="input-group-text">-</span>
+  <form @submit.prevent="submitFilters">
+    <div class="mb-3">
+      <label for="region-filter">Região</label>
+      <select
+        class="form-select"
+        multiple
+        v-model="selectedRegions"
+        id="region-filter"
+      >
+        <option value="CPT">Continente</option>
+        <option value="AZR">Açores</option>
+        <option value="MDA">Madeira</option>
+        <option value="OT">Outros</option>
+      </select>
     </div>
-    <input type="number" class="form-control" />
-  </div>
+
+    <div class="mb-3">
+      <label for="mode-filter">Modo</label>
+      <select
+        class="form-select"
+        multiple
+        v-model="selectedModes"
+        id="mode-filter"
+      >
+        <option value="fm">FM</option>
+        <option value="dstar">D-STAR</option>
+        <option value="fusion">Fusion/C4FM</option>
+        <option value="dmr">DMR</option>
+      </select>
+    </div>
+
+    <div class="mb-3">
+      <label for="band-filter">Banda</label>
+      <select
+        class="form-select"
+        multiple
+        v-model="selectedBands"
+        id="band-filter"
+      >
+        <option value="g2m">&gt; 2m</option>
+        <option value="2m">2m</option>
+        <option value="70cm">70cm</option>
+        <option value="l70cm">&lt; 70cm</option>
+      </select>
+    </div>
+
+    <div class="mb-3">
+      <button type="submit" class="btn btn-primary mb-3" onclick="this.blur();">
+        Submeter
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -56,15 +64,20 @@ export default {
   name: "Filters",
   data() {
     return {
-      selectedRegions: [],
-      selectedModes: [],
-      selectedRf: [],
-      modulation: "",
+      selectedRegions: ["CPT", "AZR", "MDA"],
+      selectedModes: ["fm", "dstar", "fusion", "dmr"],
+      selectedBands: ["2m", "70cm"],
     };
   },
 
   mounted() {
     state.getRepeaters();
+  },
+
+  methods: {
+    submitFilters() {
+      console.log("Submited!");
+    },
   },
 };
 </script>
