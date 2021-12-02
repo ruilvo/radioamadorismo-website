516f6e563d6b3eea609a843a292da8641a1e3068
Rui Oliveira <ruimail24@gmail.com>
2021-12-02 09:17:06 +0000

    Add more filters

--- frontend/src/components/repeaters/Filters.vue
+++ frontend/src/components/repeaters/Filters.vue
@@ -3,13 +3,50 @@
     <h4>Filtros</h4>
   </div>
 
+  <label for="region-filter">Região</label>
+  <select
+    class="form-select"
+    multiple
+    v-model="selectedRegions"
+    id="region-filter"
+  >
+    <option value="CPT">Continente</option>
+    <option value="AZR">Açores</option>
+    <option value="MDA">Madeira</option>
+    <option value="OT">Outros</option>
+  </select>
+
   <label for="mode-filter">Modo</label>
-  <select class="form-select" multiple v-model="selectedModes">
+  <select class="form-select" multiple v-model="selectedModes" id="mode-filter">
     <option value="fm">FM</option>
     <option value="dstar">D-STAR</option>
     <option value="fusion">Fusion/C4FM</option>
     <option value="dmr">DMR</option>
   </select>
+
+  <label for="rf-filter">RF</label>
+  <select class="form-select" multiple v-model="selectedRf" id="rf-filter">
+    <option value="simplex">Simplex</option>
+    <option value="half_duplex">Semi-duplex</option>
+  </select>
+
+  <label for="modulation-filter" class="form-label">Modulação</label>
+  <input
+    type="email"
+    class="form-control"
+    id="modulation-filter"
+    placeholder="Ex.: 11kHzF3E"
+    v-model="modulation"
+  />
+
+  <label for="freq-filter" class="form-label">Frequência</label>
+  <div class="input-group" id="freq-filter">
+    <input type="number" class="form-control" />
+    <div class="input-group-prepend">
+      <span class="input-group-text">-</span>
+    </div>
+    <input type="number" class="form-control" />
+  </div>
 </template>
 
 <script>
@@ -19,7 +56,10 @@ export default {
   name: "Filters",
   data() {
     return {
+      selectedRegions: [],
       selectedModes: [],
+      selectedRf: [],
+      modulation: "",
     };
   },
 
