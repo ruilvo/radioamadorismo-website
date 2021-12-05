107497ce71aa4aa039fa3fa4cbfa060a2e483d6c
Rui Oliveira <ruimail24@gmail.com>
2021-12-05 19:55:05 +0000

    Change the store to a module system

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore } from "vuex";
 
+// import example from './module-example'
+
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    // example
+  },
 });
