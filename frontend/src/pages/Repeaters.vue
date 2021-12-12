c34bba8c691d005d115195d100db2a46d31d64c6
Rui Oliveira <ruimail24@gmail.com>
2021-12-12 10:24:02 +0000

    Add repeaters page

---
+++ frontend/src/pages/Repeaters.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <span>Repetidores</span>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Repeaters",
+});
+</script>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,7 +2,10 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }],
+    children: [
+      { path: "", component: () => import("pages/Index.vue") },
+      { path: "repetidores/", component: () => import("pages/Repeaters.vue") },
+    ],
   },
 
   // Always leave this as last one,
