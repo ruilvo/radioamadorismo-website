5db90883fc41354349a371e908cd11d563601aae
Rui Oliveira <ruimail24@gmail.com>
2021-12-12 10:23:47 +0000

    Change index.vue to a basic thing

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <h1 class="text-center">Bem vindos!</h1>
   </q-page>
 </template>
 
