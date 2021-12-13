9abb071d62d37eac895d9bfecf88f5633e45ac0f
Rui Oliveira <ruimail24@gmail.com>
2021-12-13 20:22:40 +0000

    Add store to the component

--- frontend/src/components/repeaters/RepeatersList.vue
+++ frontend/src/components/repeaters/RepeatersList.vue
@@ -1,9 +1,19 @@
 <template>
-  <h2>Lista de repetidores</h2>
+  {{ this.repeatersStore.repeaters }}
 </template>
 
 <script>
+import { mapStores } from "pinia";
+import { useRepeatersStore } from "src/stores/repeaters";
+
 export default {
   name: "RepeatersList",
+  mounted() {
+    this.repeatersStore.updateRepeaters();
+  },
+  computed: {
+    // mapStores produces an object called [id]Store...
+    ...mapStores(useRepeatersStore), // aka this.repeatersStore
+  },
 };
 </script>
