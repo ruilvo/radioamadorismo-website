0f384dda88dd5ebb7385a53ccd2c4a2460feed5f
Rui Oliveira <ruimail24@gmail.com>
2021-12-13 20:21:20 +0000

    Get a repeaters store going

--- frontend/src/stores/store.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from "pinia";
-
-// useStore could be anything like useUser, useCart
-// the first argument is a unique id of the store across your application
-export const useStore = defineStore("store", {
-  // other options...
-});
