c7793e7b0344ee07583f28110913814520d13659
Rui Oliveira <ruimail24@gmail.com>
2021-12-16 21:44:29 +0000

    Convert to the options API

--- frontend/src/components/repeaters/RepeatersFilter.vue
+++ frontend/src/components/repeaters/RepeatersFilter.vue
@@ -40,46 +40,28 @@
 </template>
 
 <script>
-import { useQuasar } from "quasar";
-import { ref } from "vue";
-
 export default {
-  setup() {
-    const $q = useQuasar();
-
-    const name = ref(null);
-    const age = ref(null);
-    const accept = ref(false);
-
+  name: "RepeatersFilter",
+  data() {
     return {
-      name,
-      age,
-      accept,
-
-      onSubmit() {
-        if (accept.value !== true) {
-          $q.notify({
-            color: "red-5",
-            textColor: "white",
-            icon: "warning",
-            message: "You need to accept the license and terms first",
-          });
-        } else {
-          $q.notify({
-            color: "green-4",
-            textColor: "white",
-            icon: "cloud_done",
-            message: "Submitted",
-          });
-        }
-      },
-
-      onReset() {
-        name.value = null;
-        age.value = null;
-        accept.value = false;
-      },
+      name: null,
+      age: null,
+      accept: false,
     };
+  },
+  methods: {
+    onSubmit() {
+      if (this.accept.value !== true) {
+        console.log("You need to accept the license and terms first");
+      } else {
+        console.log("Submitted");
+      }
+    },
+    onReset() {
+      this.name = null;
+      this.age = null;
+      this.accept = false;
+    },
   },
 };
 </script>
