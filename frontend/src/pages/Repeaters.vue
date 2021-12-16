d7c693199c093c2604aa6364f80001c900d620c4
Rui Oliveira <ruimail24@gmail.com>
2021-12-16 15:02:05 +0000

    Load the repeaters on first rendering

--- frontend/src/pages/Repeaters.vue
+++ frontend/src/pages/Repeaters.vue
@@ -5,7 +5,17 @@
 </template>
 
 <script>
+import { mapStores } from "pinia";
+import { useRepeatersStore } from "src/stores/repeaters";
+
 export default {
   name: "Repeaters",
+  mounted() {
+    this.repeatersStore.updateRepeaters();
+  },
+  computed: {
+    // mapStores produces an object called [id]Store...
+    ...mapStores(useRepeatersStore), // aka this.repeatersStore
+  },
 };
 </script>
