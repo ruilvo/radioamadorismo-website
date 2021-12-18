3a2a85f98ac46f50e23c4fa140465fcc63557c47
Rui Oliveira <ruimail24@gmail.com>
2021-12-18 18:02:08 +0000

    Go back to en-US for the backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "pt-PT"
+LANGUAGE_CODE = "en-US"
 
 TIME_ZONE = "UTC"
 
