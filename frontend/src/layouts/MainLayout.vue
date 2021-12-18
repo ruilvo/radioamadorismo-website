5d3cf33ea9cc723dc820b502ad59ea962aac9589
Rui Oliveira <ruimail24@gmail.com>
2021-12-18 17:24:03 +0000

    Move drawer to its own object

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -12,98 +12,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      show-if-above
-      v-model="leftDrawerOpen"
-      side="left"
-      bordered
-      class="bg-grey-3"
-    >
-      <q-list>
-        <q-item clickable to="/" key="start" exact>
-          <q-item-section avatar>
-            <q-icon name="home" />
-          </q-item-section>
-          <q-item-section>Página inicial</q-item-section>
-        </q-item>
-        <q-separator key="sep1" />
-        <q-expansion-item
-          expand-separator
-          icon="cell_tower"
-          key="repeaters"
-          to="/repetidores"
-          label="Repetidores"
-          v-model="repeaters_expanded"
-        >
-          <q-item
-            clickable
-            to="/repetidores"
-            exact
-            key="repeaters-list"
-            :inset-level="1"
-          >
-            <q-item-section avatar>
-              <q-icon name="list" />
-            </q-item-section>
-            <q-item-section>Lista</q-item-section>
-          </q-item>
-          <q-item
-            clickable
-            to="/repetidores/mapa"
-            key="repeaters-map"
-            :inset-level="1"
-          >
-            <q-item-section avatar>
-              <q-icon name="map" />
-            </q-item-section>
-            <q-item-section>Mapa</q-item-section>
-          </q-item>
-        </q-expansion-item>
-        <q-separator key="sep2" />
-        <q-expansion-item
-          expand-separator
-          icon="api"
-          key="api-tab"
-          label="API"
-          v-model="api_expanded"
-        >
-          <q-item
-            clickable
-            to="/swagger"
-            exact
-            key="swagger-ui"
-            :inset-level="1"
-          >
-            <q-item-section avatar>
-              <q-icon name="description" />
-            </q-item-section>
-            <q-item-section>Documentação (🇬🇧)</q-item-section>
-          </q-item>
-          <q-item
-            clickable
-            tag="a"
-            href="/api/v1/repeaters/"
-            exact
-            key="repeaters-api"
-            :inset-level="1"
-          >
-            <q-item-section avatar>
-              <q-icon name="cell_tower" />
-            </q-item-section>
-            <q-item-section>Repetidores (🇬🇧)</q-item-section>
-            <q-item-section side
-              ><q-icon name="launch" size="xs"
-            /></q-item-section>
-          </q-item>
-        </q-expansion-item>
-        <q-item clickable key="about-url" to="/sobre">
-          <q-item-section avatar>
-            <q-icon name="help_outline" />
-          </q-item-section>
-          <q-item-section>Sobre</q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
+    <Drawer v-model="leftDrawerOpen" />
 
     <q-page-container>
       <router-view />
@@ -112,34 +21,24 @@
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from "vue";
+
+import Drawer from "src/components/drawer/Drawer.vue";
+
+export default defineComponent({
   name: "MainLayout",
-  data() {
+  components: {
+    Drawer,
+  },
+  setup() {
+    const leftDrawerOpen = ref(false);
+
     return {
-      leftDrawerOpen: false,
-      repeaters_expanded: this.$route.path.includes("/repetidores"),
-      api_expanded: this.$route.path.includes("/swagger"),
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
     };
   },
-  methods: {
-    toggleLeftDrawer() {
-      this.leftDrawerOpen = !this.leftDrawerOpen;
-    },
-  },
-  computed: {
-    currentRoutePath() {
-      return this.$route.path;
-    },
-  },
-  watch: {
-    currentRoutePath: function (to, from) {
-      if (to.includes("/repetidores")) {
-        this.repeaters_expanded = true;
-      }
-      if (to.includes("/swagger")) {
-        this.api_expanded = true;
-      }
-    },
-  },
-};
+});
 </script>
