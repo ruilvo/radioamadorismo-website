1be2c38728f007222844ead538e11c245c61116b
Rui Oliveira <ruimail24@gmail.com>
2021-12-18 16:41:41 +0000

    Remove the scroll area from the drawer

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -19,92 +19,90 @@
       bordered
       class="bg-grey-3"
     >
-      <q-scroll-area class="fit">
-        <q-list>
-          <q-item clickable to="/" key="start" exact>
-            <q-item-section avatar>
-              <q-icon name="home" />
-            </q-item-section>
-            <q-item-section>Página inicial</q-item-section>
-          </q-item>
-          <q-separator key="sep1" />
-          <q-expansion-item
-            expand-separator
-            icon="cell_tower"
-            key="repeaters"
+      <q-list>
+        <q-item clickable to="/" key="start" exact>
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>Página inicial</q-item-section>
+        </q-item>
+        <q-separator key="sep1" />
+        <q-expansion-item
+          expand-separator
+          icon="cell_tower"
+          key="repeaters"
+          to="/repetidores"
+          label="Repetidores"
+          v-model="repeaters_expanded"
+        >
+          <q-item
+            clickable
             to="/repetidores"
-            label="Repetidores"
-            v-model="repeaters_expanded"
+            exact
+            key="repeaters-list"
+            :inset-level="1"
           >
-            <q-item
-              clickable
-              to="/repetidores"
-              exact
-              key="repeaters-list"
-              :inset-level="1"
-            >
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
-              <q-item-section>Lista</q-item-section>
-            </q-item>
-            <q-item
-              clickable
-              to="/repetidores/mapa"
-              key="repeaters-map"
-              :inset-level="1"
-            >
-              <q-item-section avatar>
-                <q-icon name="map" />
-              </q-item-section>
-              <q-item-section>Mapa</q-item-section>
-            </q-item>
-          </q-expansion-item>
-          <q-separator key="sep2" />
-          <q-expansion-item
-            expand-separator
-            icon="api"
-            key="api-tab"
-            label="API"
-            v-model="api_expanded"
-          >
-            <q-item
-              clickable
-              to="/swagger"
-              exact
-              key="swagger-ui"
-              :inset-level="1"
-            >
-              <q-item-section avatar>
-                <q-icon name="description" />
-              </q-item-section>
-              <q-item-section>Documentação (🇬🇧)</q-item-section>
-            </q-item>
-            <q-item
-              clickable
-              tag="a"
-              href="/api/v1/repeaters/"
-              exact
-              key="repeaters-api"
-              :inset-level="1"
-            >
-              <q-item-section avatar>
-                <q-icon name="cell_tower" />
-              </q-item-section>
-              <q-item-section>Repetidores (🇬🇧)</q-item-section>
-              <q-item-section side
-                ><q-icon name="launch" size="xs"
-              /></q-item-section>
-            </q-item>
-          </q-expansion-item>
-          <q-item clickable key="about-url" to="/sobre">
             <q-item-section avatar>
-              <q-icon name="help_outline" />
+              <q-icon name="list" />
             </q-item-section>
-            <q-item-section>Sobre</q-item-section>
+            <q-item-section>Lista</q-item-section>
           </q-item>
-        </q-list>
-      </q-scroll-area>
+          <q-item
+            clickable
+            to="/repetidores/mapa"
+            key="repeaters-map"
+            :inset-level="1"
+          >
+            <q-item-section avatar>
+              <q-icon name="map" />
+            </q-item-section>
+            <q-item-section>Mapa</q-item-section>
+          </q-item>
+        </q-expansion-item>
+        <q-separator key="sep2" />
+        <q-expansion-item
+          expand-separator
+          icon="api"
+          key="api-tab"
+          label="API"
+          v-model="api_expanded"
+        >
+          <q-item
+            clickable
+            to="/swagger"
+            exact
+            key="swagger-ui"
+            :inset-level="1"
+          >
+            <q-item-section avatar>
+              <q-icon name="description" />
+            </q-item-section>
+            <q-item-section>Documentação (🇬🇧)</q-item-section>
+          </q-item>
+          <q-item
+            clickable
+            tag="a"
+            href="/api/v1/repeaters/"
+            exact
+            key="repeaters-api"
+            :inset-level="1"
+          >
+            <q-item-section avatar>
+              <q-icon name="cell_tower" />
+            </q-item-section>
+            <q-item-section>Repetidores (🇬🇧)</q-item-section>
+            <q-item-section side
+              ><q-icon name="launch" size="xs"
+            /></q-item-section>
+          </q-item>
+        </q-expansion-item>
+        <q-item clickable key="about-url" to="/sobre">
+          <q-item-section avatar>
+            <q-icon name="help_outline" />
+          </q-item-section>
+          <q-item-section>Sobre</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
