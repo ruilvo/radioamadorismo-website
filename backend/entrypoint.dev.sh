6984817f4acc444872506c1cd18a499d86cc171b
Rui Oliveira <ruimail24@gmail.com>
2021-12-20 10:00:05 +0000

    Add collect static to the dev server also

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -5,4 +5,5 @@ while ! nc -z $SQL_HOST $SQL_PORT; do
 done
 echo "PostgreSQL started"
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
 exec python manage.py runserver 0.0.0.0:8000
