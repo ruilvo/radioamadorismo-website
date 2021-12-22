a3346df99d93d1626a018900e804e8682b839698
Rui Oliveira <ruimail24@gmail.com>
2021-12-22 17:15:33 +0000

    Fix the devserver script

--- backend/start_devserver.sh
+++ backend/start_devserver.sh
@@ -4,6 +4,7 @@ while ! nc -z $SQL_HOST $SQL_PORT; do
     sleep 0.1
 done
 echo "PostgreSQL started"
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --clear
 exec python manage.py runserver 0.0.0.0:8000
