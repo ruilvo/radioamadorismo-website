e7de9812d59eab43d2880da71258e033c93e6ad8
Rui Oliveira <ruimail24@gmail.com>
2021-12-22 21:03:33 +0000

    Split the django migrations and dev server scripts

--- backend/run_migration_chores.sh
+++ backend/run_migration_chores.sh
@@ -4,4 +4,6 @@ while ! nc -z $SQL_HOST $SQL_PORT; do
     sleep 0.1
 done
 echo "PostgreSQL started"
-exec python manage.py runserver 0.0.0.0:8000
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --clear --noinput
