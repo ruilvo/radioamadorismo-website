07291af32bb42c6fe61ea0a3e11800a0194e5efd
Rui Oliveira <ruimail24@gmail.com>
2021-12-22 16:51:59 +0000

    Update with the old routes and components

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,7 +2,25 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }],
+    children: [
+      { path: "", component: () => import("pages/Index.vue") },
+      {
+        path: "repetidores/",
+        component: () => import("pages/Repeaters.vue"),
+        children: [
+          {
+            path: "",
+            component: () => import("components/repeaters/RepeatersList.vue"),
+          },
+          {
+            path: "mapa/",
+            component: () => import("components/repeaters/RepeatersMap.vue"),
+          },
+        ],
+      },
+      { path: "swagger/", component: () => import("pages/Swagger.vue") },
+      { path: "sobre/", component: () => import("pages/About.vue") },
+    ],
   },
 
   // Always leave this as last one,
