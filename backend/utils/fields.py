64b9bfca0cf63ad9c5392aa8c26af14801f433bd
Rui Oliveira <ruimail24@gmail.com>
2021-12-22 13:59:57 +0000

    Add HtmlRichTextFieldApiField

--- backend/utils/fields.py
+++ backend/utils/fields.py
@@ -1,5 +1,10 @@
+"""
+https://github.com/wagtail/wagtail/issues/2695
+"""
+
 from rest_framework import serializers
 
+from wagtail.api.conf import APIField
 from wagtail.core.rich_text import expand_db_html
 
 
@@ -7,3 +12,9 @@ class HtmlRichTextField(serializers.CharField):
     def to_representation(self, value):
         representation = super().to_representation(value)
         return expand_db_html(representation)
+
+
+class HtmlRichTextFieldApiField(APIField):
+    def __init__(self, name):
+        serializer = HtmlRichTextField()
+        super().__init__(name=name, serializer=serializer)
