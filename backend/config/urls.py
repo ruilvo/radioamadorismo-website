3e7a8f31c4fe9d5ef7b8a40d7434d50de62a619a
Rui Oliveira <ruimail24@gmail.com>
2021-12-23 17:30:27 +0000

    Remove useless line

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework.schemas import get_schema_view
-
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
