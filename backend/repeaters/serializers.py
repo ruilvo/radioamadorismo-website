dbbfce38f5489fa873faa4cc4c359ad0482b5e9d
Rui Oliveira <ruimail24@gmail.com>
2021-12-24 13:29:58 +0000

    Completely re-write the serializers using drf-writable-nested

--- backend/repeaters/serializers.py
+++ backend/repeaters/serializers.py
@@ -1,4 +1,11 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework import serializers
+
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from drf_writable_nested.mixins import UniqueFieldsMixin
+
+from utils.fields import HtmlRichTextSerializerField
 
 from .models import (
     DimHalfDuplex,
@@ -13,48 +20,125 @@ from .models import (
     FactRepeater,
 )
 
-from utils.fields import HtmlRichTextSerializerField
 
-
-class DimHalfDuplexSerializer(serializers.ModelSerializer):
+class DimHalfDuplexSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     shift = serializers.ReadOnlyField()
 
     class Meta:
         model = DimHalfDuplex
         fields = "__all__"
 
+    def create(self, validated_data):
+        tx_mhz = validated_data.get("tx_mhz", None)
+        rx_mhz = validated_data.get("rx_mhz", None)
+        channel = validated_data.get("channel", None)
+        new_object, new_object_created = DimHalfDuplex.objects.get_or_create(
+            tx_mhz=tx_mhz, rx_mhz=rx_mhz, defaults=validated_data
+        )
+        if not new_object_created and channel is not None:
+            new_object.channel = channel
+            new_object.save()
+        return new_object
 
-class DimSimplexSerializer(serializers.ModelSerializer):
+
+class DimSimplexSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimSimplex
         fields = "__all__"
 
+    def create(self, validated_data):
+        freq_mhz = validated_data.get("freq_mhz", None)
+        channel = validated_data.get("channel", None)
+        new_object, new_object_created = DimSimplex.objects.get_or_create(
+            freq_mhz=freq_mhz, defaults=validated_data
+        )
+        if not new_object_created and channel is not None:
+            new_object.channel = channel
+            new_object.save()
+        return new_object
 
-class DimFmSerializer(serializers.ModelSerializer):
+
+class DimFmSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimFm
         fields = "__all__"
 
+    def create(self, validated_data):
+        modulation = validated_data.get("modulation", None)
+        tone = validated_data.get("tone", None)
+        if modulation is not None and tone is not None:
+            new_object, _ = DimFm.objects.get_or_create(
+                modulation=modulation, tone=tone, defaults=validated_data
+            )
+            return new_object
+        return DimFm.objects.create(**validated_data)
 
-class DimDStarSerializer(serializers.ModelSerializer):
+
+class DimDStarSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimDStar
         fields = "__all__"
 
+    def create(self, validated_data):
+        modulation = validated_data.get("modulation", None)
+        gateway = validated_data.get("gateway", None)
+        reflector = validated_data.get("reflector", None)
+        if gateway is not None and reflector is not None:
+            new_object, new_object_created = DimDStar.objects.get_or_create(
+                gateway=gateway,
+                reflector=reflector,
+                defaults=validated_data,
+            )
+            if not new_object_created and modulation is not None:
+                new_object.modulation = modulation
+                new_object.save()
+            return new_object
+        return DimDStar.objects.create(**validated_data)
 
-class DimFusionSerializer(serializers.ModelSerializer):
+
+class DimFusionSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimFusion
         fields = "__all__"
 
+    def create(self, validated_data):
+        modulation = validated_data.get("modulation", None)
+        wiresx = validated_data.get("wiresx", None)
+        room_id = validated_data.get("room_id", None)
+        if wiresx is not None and room_id is not None:
+            new_object, new_object_created = DimFusion.objects.get_or_create(
+                wiresx=wiresx,
+                room_id=room_id,
+                defaults=validated_data,
+            )
+            if not new_object_created and modulation is not None:
+                new_object.modulation = modulation
+                new_object.save()
+            return new_object
+        return DimFusion.objects.create(**validated_data)
 
-class DimDmrTgSerializer(serializers.ModelSerializer):
+
+class DimDmrTgSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimDmrTg
         fields = "__all__"
 
+    def create(self, validated_data):
+        name = validated_data.get("name", None)
+        dmr_id = validated_data.get("dmr_id", None)
+        if dmr_id is not None:
+            new_object, new_object_created = DimDmrTg.objects.get_or_create(
+                dmr_id=dmr_id,
+                defaults=validated_data,
+            )
+            if not new_object_created:
+                new_object.name = name
+                new_object.save()
+            return new_object
+        return DimDmrTg.objects.create(**validated_data)
 
-class DimDmrSerializer(serializers.ModelSerializer):
+
+class DimDmrSerializer(UniqueFieldsMixin, WritableNestedModelSerializer):
     ts1_default_tg = DimDmrTgSerializer(many=False, required=False)
     ts2_default_tg = DimDmrTgSerializer(many=False, required=False)
     ts1_alternative_tgs = DimDmrTgSerializer(many=True, required=False)
@@ -65,172 +149,67 @@ class DimDmrSerializer(serializers.ModelSerializer):
         model = DimDmr
         fields = "__all__"
 
+    def create(self, validated_data):
+        try:
+            return DimDmr.objects.get(dmr_id=validated_data["dmr_id"])
+        except ObjectDoesNotExist:
+            return super(DimDmrSerializer, self).create(validated_data)
 
-class DimHolderSerializer(serializers.ModelSerializer):
+
+class DimHolderSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimHolder
         fields = "__all__"
 
+    def create(self, validated_data):
+        abrv = validated_data.get("abrv", None)
+        name = validated_data.get("name", None)
+        sysop = validated_data.get("sysop", None)
+        if abrv is not None:
+            new_object, new_object_created = DimHolder.objects.get_or_create(
+                abrv=abrv,
+                defaults=validated_data,
+            )
+            if not new_object_created:
+                if name is not None:
+                    new_object.name = name
+                if sysop is not None:
+                    new_object.sysop = sysop
+                new_object.save()
+            return new_object
+        return DimHolder.objects.create(**validated_data)
 
-class DimLocationSerializer(serializers.ModelSerializer):
+
+class DimLocationSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = DimLocation
         fields = "__all__"
 
-
-def get_update_DimHalfDuplex(info_half_duplex_data: dict) -> DimHalfDuplex or None:
-    if info_half_duplex_data is not None:
-        (
-            info_half_duplex,
-            info_half_duplex_created,
-        ) = DimHalfDuplex.objects.get_or_create(
-            tx_mhz=info_half_duplex_data["tx_mhz"],
-            rx_mhz=info_half_duplex_data["rx_mhz"],
-            defaults=info_half_duplex_data,
-        )
-        if (
-            not info_half_duplex_created
-            and info_half_duplex_data["channel"] is not None
-        ):
-            info_half_duplex.channel = info_half_duplex_data["channel"]
-            info_half_duplex.save()
-        return info_half_duplex
-    return None
-
-
-def get_update_DimSimplex(info_simplex_data: dict) -> DimSimplex or None:
-    if info_simplex_data is not None:
-        info_simplex, info_simplex_created = DimSimplex.objects.get_or_create(
-            freq_mhz=info_simplex_data["freq_mhz"], defaults=info_simplex_data
-        )
-        if not info_simplex_created and info_simplex_data["channel"] is not None:
-            info_simplex.channel = info_simplex_data["channel"]
-            info_simplex.save()
-        return info_simplex
-    return None
+    def create(self, validated_data):
+        latitude = validated_data.get("latitude", None)
+        longitude = validated_data.get("longitude", None)
+        region = validated_data.get("region", None)
+        place = validated_data.get("place", None)
+        qth_loc = validated_data.get("qth_loc", None)
+        if latitude is not None and longitude is not None:
+            new_object, new_object_created = DimLocation.objects.get_or_create(
+                latitude=latitude,
+                longitude=longitude,
+                defaults=validated_data,
+            )
+            if not new_object_created:
+                if region is not None:
+                    new_object.region = region
+                if place is not None:
+                    new_object.place = place
+                if qth_loc is not None:
+                    new_object.qth_loc = qth_loc
+                new_object.save()
+            return new_object
+        return DimLocation.objects.create(**validated_data)
 
 
-def get_update_DimFm(info_fm_data: dict) -> DimFm or None:
-    if info_fm_data is not None:
-        info_fm, _ = DimFm.objects.get_or_create(
-            modulation=info_fm_data["modulation"],
-            tone=info_fm_data["tone"],
-            defaults=info_fm_data,
-        )
-        return info_fm
-    return None
-
-
-def get_update_DimDStar(info_dstar_data: dict) -> DimDStar or None:
-    if info_dstar_data is not None:
-        info_dstar, _ = DimDStar.objects.get_or_create(
-            modulation=info_dstar_data["modulation"],
-            gateway=info_dstar_data["gateway"],
-            reflector=info_dstar_data["reflector"],
-            defaults=info_dstar_data,
-        )
-        return info_dstar
-    return None
-
-
-def get_update_DimFusion(info_fusion_data: dict) -> DimFusion or None:
-    if info_fusion_data is not None:
-        info_fusion, _ = DimDStar.objects.get_or_create(
-            wiresx=info_fusion_data["wiresx"],
-            room_id=info_fusion_data["room_id"],
-            defaults=info_fusion_data,
-        )
-        return info_fusion
-    return None
-
-
-def get_update_DimDmrTg(tg_data: dict) -> DimDmrTg or None:
-    if tg_data is not None:
-        tg, _ = DimDmrTg.objects.get_or_create(
-            dmr_id=tg_data["dmr_id"], defaults=tg_data
-        )
-        return tg
-    return None
-
-
-def get_update_DimDmr(info_dmr_data: dict) -> DimDmr or None:
-    if info_dmr_data is not None:
-        ts1_default_tg = get_update_DimDmrTg(info_dmr_data.pop("ts1_default_tg", None))
-        ts2_default_tg = get_update_DimDmrTg(info_dmr_data.pop("ts2_default_tg", None))
-        ts1_alternative_tgs_data = info_dmr_data.pop("ts1_alternative_tgs", None)
-        ts1_alternative_tgs = []
-        if ts1_alternative_tgs_data is not None:
-            for ts1_alternative_tg_data in ts1_alternative_tgs_data:
-                ts1_alternative_tgs.append(get_update_DimDmrTg(ts1_alternative_tg_data))
-        ts2_alternative_tgs_data = info_dmr_data.pop("ts2_alternative_tgs", None)
-        ts2_alternative_tgs = []
-        if ts2_alternative_tgs_data is not None:
-            for ts2_alternative_tg_data in ts2_alternative_tgs_data:
-                ts2_alternative_tgs.append(get_update_DimDmrTg(ts2_alternative_tg_data))
-        info_dmr, info_dmr_created = DimDmr.objects.get_or_create(
-            dmr_id=info_dmr_data["dmr_id"],
-            defaults=info_dmr_data,
-        )
-        if not info_dmr_created:
-            # Check for the optional fields that are to be updated
-            if info_dmr_data["modulation"] is not None:
-                info_dmr.modulation = info_dmr_data["modulation"]
-            if info_dmr_data["color_code"] is not None:
-                info_dmr.color_code = info_dmr_data["color_code"]
-        if ts1_default_tg:
-            info_dmr.ts1_default_tg = ts1_default_tg
-        if ts2_default_tg:
-            info_dmr.ts2_default_tg = ts2_default_tg
-        if len(ts1_alternative_tgs):
-            info_dmr.ts1_alternative_tgs.clear()
-            for elem in ts1_alternative_tgs:
-                info_dmr.ts1_alternative_tgs.add(elem)
-        if len(ts2_alternative_tgs):
-            info_dmr.ts2_alternative_tgs.clear()
-            for elem in ts2_alternative_tgs:
-                info_dmr.ts2_alternative_tgs.add(elem)
-        info_dmr.save()
-        return info_dmr
-    return None
-
-
-def get_update_DimHolder(info_holder_data: dict) -> DimHolder or None:
-    if info_holder_data is not None:
-        info_holder, info_holder_created = DimHolder.objects.get_or_create(
-            abrv=info_holder_data["abrv"],
-            defaults=info_holder_data,
-        )
-        if not info_holder_created:
-            if info_holder_data["name"] is not None:
-                info_holder.name = info_holder_data["name"]
-            if info_holder_data["sysop"] is not None:
-                info_holder.sysop = info_holder_data["sysop"]
-            info_holder.save()
-        return info_holder
-    return None
-
-
-def get_update_DimLocation(info_location_data: dict) -> DimLocation or None:
-    if info_location_data is not None:
-        (info_location, info_location_created,) = DimLocation.objects.get_or_create(
-            latitude=info_location_data["latitude"],
-            longitude=info_location_data["longitude"],
-            defaults=info_location_data,
-        )
-        if not info_location_created:
-            # Check for the optional fields that are to be updated
-            if info_location_data["region"] is not None:
-                info_location.region = info_location_data["region"]
-            if info_location_data["place"] is not None:
-                info_location.place = info_location_data["place"]
-            if info_location_data["qth_loc"] is not None:
-                info_location.qth_loc = info_location_data["qth_loc"]
-            info_location.save()
-        return info_location
-    return None
-
-
-class FactRepeaterSerializer(serializers.ModelSerializer):
+class FactRepeaterSerializer(WritableNestedModelSerializer):
     notes = HtmlRichTextSerializerField(required=False)
 
     info_half_duplex = DimHalfDuplexSerializer(many=False, required=False)
@@ -245,86 +224,3 @@ class FactRepeaterSerializer(serializers.ModelSerializer):
     class Meta:
         model = FactRepeater
         fields = "__all__"
-
-    # https://www.django-rest-framework.org/api-guide/relations/#writable-nested-serializers
-    # I should write a test for this one...
-    def create(self, validated_data):
-
-        info_half_duplex = get_update_DimHalfDuplex(
-            validated_data.pop("info_half_duplex", None)
-        )
-        info_simplex = get_update_DimSimplex(validated_data.pop("info_simplex", None))
-        info_fm = get_update_DimFm(validated_data.pop("info_fm", None))
-        info_dstar = get_update_DimDStar(validated_data.pop("info_dstar", None))
-        info_fusion = get_update_DimFusion(validated_data.pop("info_fusion", None))
-        info_dmr = get_update_DimDmr(validated_data.pop("info_dmr", None))
-        info_holder = get_update_DimHolder(validated_data.pop("info_holder", None))
-        info_location = get_update_DimLocation(
-            validated_data.pop("info_location", None)
-        )
-
-        repeater = FactRepeater.objects.create(
-            info_half_duplex=info_half_duplex,
-            info_simplex=info_simplex,
-            info_fm=info_fm,
-            info_fusion=info_fusion,
-            info_dstar=info_dstar,
-            info_dmr=info_dmr,
-            info_holder=info_holder,
-            info_location=info_location,
-            **validated_data,
-        )
-
-        return repeater
-
-    def update(self, instance, validated_data):
-
-        info_half_duplex = get_update_DimHalfDuplex(
-            validated_data.pop("info_half_duplex", None)
-        )
-        instance.info_half_duplex = (
-            info_half_duplex
-            if info_half_duplex is not None
-            else instance.info_half_duplex
-        )
-
-        info_simplex = get_update_DimSimplex(validated_data.pop("info_simplex", None))
-        instance.info_simplex = (
-            info_simplex if info_simplex is not None else instance.info_simplex
-        )
-
-        info_fm = get_update_DimFm(validated_data.pop("info_fm", None))
-        instance.info_fm = info_fm if info_fm is not None else instance.info_fm
-
-        info_dstar = get_update_DimDStar(validated_data.pop("info_dstar", None))
-        instance.info_dstar = (
-            info_dstar if info_dstar is not None else instance.info_dstar
-        )
-
-        info_fusion = get_update_DimFusion(validated_data.pop("info_fusion", None))
-        instance.info_fusion = (
-            info_fusion if info_fusion is not None else instance.info_fusion
-        )
-
-        info_dmr = get_update_DimDmr(validated_data.pop("info_dmr", None))
-        instance.info_dmr = info_dmr if info_dmr is not None else instance.info_dmr
-
-        info_holder = get_update_DimHolder(validated_data.pop("info_holder", None))
-        instance.info_holder = (
-            info_holder if info_holder is not None else instance.info_holder
-        )
-
-        info_location = get_update_DimLocation(
-            validated_data.pop("info_location", None)
-        )
-        instance.info_location = (
-            info_location if info_location is not None else instance.info_location
-        )
-
-        instance.callsign = validated_data.get("callsign", instance.callsign)
-        instance.notes = validated_data.get("notes", instance.notes)
-        instance.pwr_w = validated_data.get("pwr_w", instance.pwr_w)
-
-        instance.save()
-
-        return instance
