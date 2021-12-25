39000a3e498dcdcaed22840ce264497f592680fd
Rui Oliveira <ruimail24@gmail.com>
2021-12-25 18:09:11 +0000

    Add basic auth store

---
+++ frontend/src/stores/auth.js
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+
+import { api } from "boot/axios";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    username: null,
+    token: null,
+  }),
+  actions: {
+    async login({ username, password }) {
+      try {
+        var response = await api.post("/api/auth/dj-rest-auth/login/", {
+          username,
+          password,
+        });
+        this.username = username;
+        this.token = response.data.key;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async logout() {
+      try {
+        await api.post("/api/auth/dj-rest-auth/logout/");
+        this.username = null;
+        this.token = null;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  getters: {
+    isLoggedIn: (state) => !!state.token,
+  },
+});
