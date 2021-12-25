c4fb9196941696107d276639d594c29b0d2d19c1
Rui Oliveira <ruimail24@gmail.com>
2021-12-25 20:50:48 +0000

    Force login on API 401s

--- frontend/src/boot/interceptors.js
+++ frontend/src/boot/interceptors.js
@@ -4,10 +4,7 @@ import { useRouter } from "vue-router";
 
 import axios from "axios";
 
-import { useAuthStore } from "src/stores/auth";
-
 export default boot(() => {
-  const authStore = useAuthStore();
   const router = useRouter();
 
   axios.interceptors.response.use(undefined, function (error) {
@@ -15,7 +12,6 @@ export default boot(() => {
       const originalRequest = error.config;
       if (error.response.status === 401 && !originalRequest._retry) {
         originalRequest._retry = true;
-        authStore.reset();
         return router.push("/login");
       }
     }
