132e01f1d80568e6065dedadb8e57e2f87a2f986
Rui Oliveira <ruimail24@gmail.com>
2021-12-27 15:24:21 +0000

    Serialize all models

--- backend/repeaters/urls.py
+++ backend/repeaters/urls.py
@@ -1,9 +1,29 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import FactRepeaterViewSet
+from .views import (
+    DimHalfDuplexViewSet,
+    DimSimplexViewSet,
+    DimFmViewSet,
+    DimDStarViewSet,
+    DimFusionViewSet,
+    DimDmrTgViewSet,
+    DimDmrViewSet,
+    DimHolderViewSet,
+    DimLocationViewSet,
+    FactRepeaterViewSet,
+)
 
 
 router = SimpleRouter()
-router.register("", FactRepeaterViewSet, basename="repeaters")
+router.register("fact-repeaters", FactRepeaterViewSet, basename="fact-repeaters")
+router.register("dim-half-duplex", DimHalfDuplexViewSet, basename="dim-half-duplex")
+router.register("dim-simplex", DimSimplexViewSet, basename="dim-simplex")
+router.register("dim-fm", DimFmViewSet, basename="dim-fm")
+router.register("dim-dstar", DimDStarViewSet, basename="dim-dstar")
+router.register("dim-fusion", DimFusionViewSet, basename="dim-fusion")
+router.register("dim-dmr-tg", DimDmrTgViewSet, basename="dim-dmr-tg")
+router.register("dim-dmr", DimDmrViewSet, basename="dim-dmr")
+router.register("dim-holder", DimHolderViewSet, basename="dim-holder")
+router.register("dim-location", DimLocationViewSet, basename="dim-location")
 
 urlpatterns = router.urls
--- backend/repeaters/views.py
+++ backend/repeaters/views.py
@@ -1,6 +1,29 @@
 from rest_framework import viewsets
-from .models import FactRepeater
-from .serializers import FactRepeaterSerializer
+
+from .models import (
+    DimHalfDuplex,
+    DimSimplex,
+    DimFm,
+    DimDStar,
+    DimFusion,
+    DimDmrTg,
+    DimDmr,
+    DimHolder,
+    DimLocation,
+    FactRepeater,
+)
+from .serializers import (
+    DimHalfDuplexSerializer,
+    DimSimplexSerializer,
+    DimFmSerializer,
+    DimDStarSerializer,
+    DimFusionSerializer,
+    DimDmrTgSerializer,
+    DimDmrSerializer,
+    DimHolderSerializer,
+    DimLocationSerializer,
+    FactRepeaterSerializer,
+)
 
 from .filters import FactRepeaterFilter
 
@@ -11,3 +34,57 @@ class FactRepeaterViewSet(viewsets.ModelViewSet):
     serializer_class = FactRepeaterSerializer
 
     filterset_class = FactRepeaterFilter
+
+
+class DimHalfDuplexViewSet(viewsets.ModelViewSet):
+
+    queryset = DimHalfDuplex.objects.all()
+    serializer_class = DimHalfDuplexSerializer
+
+
+class DimSimplexViewSet(viewsets.ModelViewSet):
+
+    queryset = DimSimplex.objects.all()
+    serializer_class = DimSimplexSerializer
+
+
+class DimFmViewSet(viewsets.ModelViewSet):
+
+    queryset = DimFm.objects.all()
+    serializer_class = DimFmSerializer
+
+
+class DimDStarViewSet(viewsets.ModelViewSet):
+
+    queryset = DimDStar.objects.all()
+    serializer_class = DimDStarSerializer
+
+
+class DimFusionViewSet(viewsets.ModelViewSet):
+
+    queryset = DimFusion.objects.all()
+    serializer_class = DimFusionSerializer
+
+
+class DimDmrTgViewSet(viewsets.ModelViewSet):
+
+    queryset = DimDmrTg.objects.all()
+    serializer_class = DimDmrTgSerializer
+
+
+class DimDmrViewSet(viewsets.ModelViewSet):
+
+    queryset = DimDmr.objects.all()
+    serializer_class = DimDmrSerializer
+
+
+class DimHolderViewSet(viewsets.ModelViewSet):
+
+    queryset = DimHolder.objects.all()
+    serializer_class = DimHolderSerializer
+
+
+class DimLocationViewSet(viewsets.ModelViewSet):
+
+    queryset = DimLocation.objects.all()
+    serializer_class = DimLocationSerializer
