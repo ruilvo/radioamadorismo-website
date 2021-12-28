d31f91e4f7e25f0a53c8343163115915aad4dd1c
Rui Oliveira <ruimail24@gmail.com>
2021-12-28 16:19:51 +0000

    Add basic posts list

---
+++ frontend/src/components/cms/blog/BlogListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card class="row justify-between items-center q-ma-sm hover:tw-bg-gray-300">
+    <q-card-section class="col">
+      <div class="text-h5 q-mt-sm q-mb-xs">{{ post.id }}: {{ post.title }}</div>
+      <div>Adicionado em {{ added }}</div>
+    </q-card-section>
+
+    <q-card-actions class="col-auto column">
+      <q-btn flat><q-icon name="edit" />Editar</q-btn>
+      <q-btn flat><q-icon name="delete" />Apagar</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent, computed } from "vue";
+
+export default defineComponent({
+  name: "BlogListItem",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const added = computed(() => {
+      return new Date(props.post.added).toLocaleString();
+    });
+
+    return {
+      added,
+    };
+  },
+});
+</script>
--- frontend/src/pages/cms/blog/BlogPostList.vue
+++ frontend/src/pages/cms/blog/BlogPostList.vue
@@ -1,13 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1 class="text-center">Lista de posts do blog</h1>
+  <q-page class="row-auto">
+    <h1 class="col-12 text-center">Lista de posts do blog</h1>
+    <BlogListItem v-for="post in posts" :key="'post' + post.id" :post="post" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+
+import useCmsBlogStore from "src/stores/cms/blog";
+
+import BlogListItem from "components/cms/blog/BlogListItem";
 
 export default defineComponent({
   name: "BlogPostList",
+  components: {
+    BlogListItem,
+  },
+  setup() {
+    const cmsBlogStore = useCmsBlogStore();
+
+    cmsBlogStore.updatePosts();
+
+    const posts = computed(() => {
+      return cmsBlogStore.posts;
+    });
+
+    return {
+      posts,
+    };
+  },
 });
 </script>
