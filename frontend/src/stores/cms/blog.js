62d33a4a1dea78f682176cb1a3ac3f526de36847
Rui Oliveira <ruimail24@gmail.com>
2021-12-29 22:32:18 +0000

    Add get delete and patch methods

--- frontend/src/stores/cms/blog.js
+++ frontend/src/stores/cms/blog.js
@@ -17,6 +17,15 @@ export const useCmsBlogStore = defineStore("cms-blog", {
           console.error(err);
         });
     },
+    getPost(id) {
+      return api.get(`/api/v1/cms/fact-blog-post/${id}`);
+    },
+    deletePost(id) {
+      return api.delete(`/api/v1/cms/fact-blog-post/${id}`);
+    },
+    updatePost(id) {
+      return api.patch(`/api/v1/cms/fact-blog-post/${id}`);
+    },
   },
 });
 
