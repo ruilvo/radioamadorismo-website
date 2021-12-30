aa9c15ecba3d5645bd1d0ecb0c3e2a7e6c3eec54
Rui Oliveira <ruimail24@gmail.com>
2021-12-30 16:33:58 +0000

    Use more sensible default styles for the headers

--- frontend/src/pages/portal/Index.vue
+++ frontend/src/pages/portal/Index.vue
@@ -1,7 +1,13 @@
 <template>
-  <q-page class="flex flex-center column">
-    <h1 class="text-center">Bem-vindos(as)!</h1>
-    <h4 class="text-center">Em construção...</h4>
+  <q-page padding>
+    <h2>Bem vindo(a) ao Portal do Radioamadorismo</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+      praesentium molestias a adipisci, dolore vitae odit, quidem consequatur
+      optio voluptates asperiores pariatur eos numquam rerum delectus commodi
+      perferendis voluptate?
+    </p>
+    <hr />
   </q-page>
 </template>
 
