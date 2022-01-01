a1570032fa9dc8299c677449da5c02ed4d4916a9
Rui Oliveira <ruimail24@gmail.com>
2022-01-01 18:07:55 +0000

    Add API documentation to get_passcode

--- backend/aprs/views.py
+++ backend/aprs/views.py
@@ -1,11 +1,20 @@
+from rest_framework import response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
+
 from .passcode_generator import passcode_generator
 
 
+@extend_schema(
+    parameters=[
+        OpenApiParameter(name="callsign", required=True, type=str),
+    ],
+    description="Get an APRS-IS passcode for a given callsign",
+)
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def get_passcode(request):
