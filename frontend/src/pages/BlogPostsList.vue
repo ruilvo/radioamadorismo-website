22b212406e6114668786ab117d838fecb58c20cc
Rui Oliveira <ruimail24@gmail.com>
2022-01-09 15:16:39 +0000

    Add a blog posts list page

---
+++ frontend/src/pages/BlogPostsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 style="margin-bottom: 15px">Notícias e posts</h2>
+  <BlogPosts />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+import BlogPosts from "components/BlogPosts.vue";
+
+export default defineComponent({
+  name: "BlogPostsList",
+  components: {
+    BlogPosts,
+  },
+});
+</script>
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,19 +1,18 @@
 <template>
   <h2>Bem vindo(a) ao Portal do Radioamadorismo</h2>
   <hr />
-  <h2 style="margin-bottom: 15px">Notícias e posts</h2>
-  <BlogPosts />
+  <BlogPostsList />
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
-import BlogPosts from "components/BlogPosts.vue";
+import BlogPostsList from "./BlogPostsList.vue";
 
 export default defineComponent({
   name: "Index",
   components: {
-    BlogPosts,
+    BlogPostsList,
   },
 });
 </script>
