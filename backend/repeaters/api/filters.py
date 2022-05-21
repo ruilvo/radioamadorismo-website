039cef5de0969415909f45aa1f4ad825caadf94b
Rui Oliveira <ruimail24@gmail.com>
2022-05-21 15:41:20 +0100

    Fix bad import

--- backend/repeaters/api/filters.py
+++ backend/repeaters/api/filters.py
@@ -1,4 +1,4 @@
-from django_filters import rest_framework as drf_filters
+from django_filters.rest_framework import filters, FilterSet
 
 from repeaters.models import FactRepeater
 from repeaters.common.filters import (
@@ -14,27 +14,23 @@ from repeaters.common.filters import (
 )
 
 
-class FactRepeaterFilter(drf_filters.FilterSet):
+class FactRepeaterFilter(FilterSet):
     # https://stackoverflow.com/a/62878113/5168563
-    modulation = drf_filters.filters.CharFilter(
-        label="modulation", method=modulation_search
-    )
-    holder = drf_filters.filters.CharFilter(label="holder", method=holder_search)
+    modulation = filters.CharFilter(label="modulation", method=modulation_search)
+    holder = filters.CharFilter(label="holder", method=holder_search)
 
-    mode = drf_filters.filters.CharFilter(label="mode", method=mode_search)
-    rf = drf_filters.filters.CharFilter(label="rf", method=rf_search)
+    mode = filters.CharFilter(label="mode", method=mode_search)
+    rf = filters.CharFilter(label="rf", method=rf_search)
 
-    freq_mhz = drf_filters.filters.NumberFilter(
-        label="freq_mhz", method=freq_mhz_search
-    )
-    freq_mhz__gte = drf_filters.filters.NumberFilter(
+    freq_mhz = filters.NumberFilter(label="freq_mhz", method=freq_mhz_search)
+    freq_mhz__gte = filters.NumberFilter(
         label="freq_mhz__gte", method=freq_mhz_search__gte
     )
-    freq_mhz__lte = drf_filters.filters.NumberFilter(
+    freq_mhz__lte = filters.NumberFilter(
         label="freq_mhz__lte", method=freq_mhz_search__lte
     )
 
-    region = drf_filters.filters.CharFilter(label="region", method=region_search)
+    region = filters.CharFilter(label="region", method=region_search)
 
     class Meta:
         model = FactRepeater
