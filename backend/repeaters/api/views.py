323cb2d9243c242b68cea4c8b2c3cd8083c7456c
Rui Oliveira <ruimail24@gmail.com>
2023-04-27 12:08:37 +0100

    Improve the viewsets' code

--- backend/repeaters/api/views.py
+++ backend/repeaters/api/views.py
@@ -30,7 +30,6 @@ from repeaters.api.filters import FactRepeaterFilter
 
 
 class FactRepeaterViewSet(viewsets.ModelViewSet):
-
     queryset = FactRepeater.objects.all()
     serializer_class = FactRepeaterSerializer
 
@@ -42,72 +41,63 @@ class FactRepeaterViewSet(viewsets.ModelViewSet):
         single model instance otherwise.
         """
         data = request.data
+        to_return = None
+        headers = {}
         if isinstance(data, list):
-            to_return = []
-            for item in data:
-                serializer = self.get_serializer(data=item)
-                serializer.is_valid(raise_exception=True)
-                serializer.save()
-                to_return.append(serializer.data)
-            return Response(to_return, status=status.HTTP_201_CREATED, headers={})
-        serializer = self.get_serializer(data=data)
+            to_return = [self._create_one_instance(item).data for item in data]
+        else:
+            serializer = self._create_one_instance(data)
+            to_return = serializer.data
+            headers = self.get_success_headers(to_return)
+        return Response(to_return, status=status.HTTP_201_CREATED, headers=headers)
+
+    def _create_one_instance(self, item):
+        serializer = self.get_serializer(data=item)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
+        return serializer
 
 
 class DimHalfDuplexViewSet(viewsets.ModelViewSet):
-
     queryset = DimHalfDuplex.objects.all()
     serializer_class = DimHalfDuplexSerializer
 
 
 class DimSimplexViewSet(viewsets.ModelViewSet):
-
     queryset = DimSimplex.objects.all()
     serializer_class = DimSimplexSerializer
 
 
 class DimFmViewSet(viewsets.ModelViewSet):
-
     queryset = DimFm.objects.all()
     serializer_class = DimFmSerializer
 
 
 class DimDStarViewSet(viewsets.ModelViewSet):
-
     queryset = DimDStar.objects.all()
     serializer_class = DimDStarSerializer
 
 
 class DimFusionViewSet(viewsets.ModelViewSet):
-
     queryset = DimFusion.objects.all()
     serializer_class = DimFusionSerializer
 
 
 class DimDmrTgViewSet(viewsets.ModelViewSet):
-
     queryset = DimDmrTg.objects.all()
     serializer_class = DimDmrTgSerializer
 
 
 class DimDmrViewSet(viewsets.ModelViewSet):
-
     queryset = DimDmr.objects.all()
     serializer_class = DimDmrSerializer
 
 
 class DimHolderViewSet(viewsets.ModelViewSet):
-
     queryset = DimHolder.objects.all()
     serializer_class = DimHolderSerializer
 
 
 class DimLocationViewSet(viewsets.ModelViewSet):
-
     queryset = DimLocation.objects.all()
     serializer_class = DimLocationSerializer
