ce6820c594b10b0e86e7900b9809742378aed685
Rui Oliveira <ruimail24@gmail.com>
2023-05-03 21:30:34 +0100

    Update routes

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -4,11 +4,14 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        name: 'home',
+        path: '',
+        component: () => import('pages/IndexPage.vue'),
+      },
+    ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
