1939647fd846a77d228850770ceaec8da2fa9c83
Rui Oliveira <ruimail24@gmail.com>
2023-05-29 21:15:08 +0000

    Fix gunicorn deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi:application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
