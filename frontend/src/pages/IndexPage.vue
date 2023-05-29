b3d321022f0a40074f2fdacb85be159cb3b6d781
Rui Oliveira <ruimail24@gmail.com>
2023-05-29 22:31:33 +0100

    Fix the tags

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,9 +1,11 @@
 <template>
-  <h2>Bem-vindo(a) à pagina de serviços do Portal do Radioamadorismo</h2>
+  <h3>Bem-vindo(a) à pagina de serviços do Portal do Radioamadorismo</h3>
   <hr />
-  Nesta página encontram-se serviços que por razões de ordem técnica não podem
-  ser integrados com o resto da plataforma. Escolha no menu do lado esquerdo o
-  serviço a que pretende aceder.
+  <p>
+    Nesta página encontram-se serviços que por razões de ordem técnica não podem
+    ser integrados com o resto da plataforma. Escolha no menu do lado esquerdo o
+    serviço a que pretende aceder.
+  </p>
 </template>
 
 <script lang="ts">
