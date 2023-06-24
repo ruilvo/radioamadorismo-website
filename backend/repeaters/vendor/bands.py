35dd6b93b95437995adceb8e55b4875e8558ac85
Rui Oliveira <ruimail24@gmail.com>
2023-06-24 13:31:14 +0000

    Add a definition for ham bands

---
+++ backend/repeaters/vendor/bands.py
@@ -0,0 +1,19 @@
+"""
+Definition of ham radio bands
+"""
+
+class Band10m:
+    min = 28.0
+    max = 29.7
+
+class Band2m:
+    min = 144.0
+    max = 146.0
+
+class Band70cm:
+    min = 430.0
+    max = 440.0
+
+class Band23cm:
+    min = 1240.0
+    max = 1300.0
