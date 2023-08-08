14f21362014e5fbf24b73ef8583fca8ea9d61165
Rui Oliveira <ruimail24@gmail.com>
2023-08-08 15:57:11 +0000

    Add export views

---
+++ backend/repeaters/views.py
@@ -0,0 +1,52 @@
+import io
+
+from django.views.decorators.http import require_safe
+from django.http import HttpRequest, HttpResponse
+
+from repeaters.vendor.exports.anytone_d878uviip import (
+    DimDmrTgAnytoneUVIIPlusSerializer,
+    ReceiveGroupsAnytoneUVIIPlusSerializer,
+    ChannelAnytoneUVIIPlusSerializer,
+    ZoneAnytoneUVIIPlusSerializer,
+)
+
+
+def generate_csv_response(filename: str, content: io.StringIO) -> HttpResponse:
+    response = HttpResponse(content.getvalue(), content_type="text/csv")
+    response["Content-Disposition"] = f'attachment;filename="{filename}"'
+
+    return response
+
+
+@require_safe
+def d878uvii_tgs_view(_: HttpRequest):
+    return generate_csv_response(
+        "TalkGroups.csv", DimDmrTgAnytoneUVIIPlusSerializer().generate_csv()
+    )
+
+
+@require_safe
+def d878uvii_rgs_view(_: HttpRequest):
+    return generate_csv_response(
+        "ReceiveGroupCallList.csv",
+        ReceiveGroupsAnytoneUVIIPlusSerializer(
+            DimDmrTgAnytoneUVIIPlusSerializer()
+        ).generate_csv(),
+    )
+
+
+@require_safe
+def d878uvii_channels_view(_: HttpRequest):
+    return generate_csv_response(
+        "Channel.csv", ChannelAnytoneUVIIPlusSerializer().generate_csv()
+    )
+
+
+@require_safe
+def d878uvii_zones_view(_: HttpRequest):
+    return generate_csv_response(
+        "Zone.csv",
+        ZoneAnytoneUVIIPlusSerializer(
+            ChannelAnytoneUVIIPlusSerializer()
+        ).generate_csv(),
+    )
