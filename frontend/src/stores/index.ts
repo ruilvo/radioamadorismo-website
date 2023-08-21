db479ffe6a9ef9513f33160a5dfd7bf61d42b404
Rui Oliveira <ruimail24@gmail.com>
2023-08-21 21:00:23 +0000

    Delete useless comments

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -15,15 +15,6 @@ declare module 'pinia' {
   }
 }
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
 export default store((/* { ssrContext } */) => {
   const pinia = createPinia();
 
