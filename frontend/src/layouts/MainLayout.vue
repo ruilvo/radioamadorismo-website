a08f6b60384ba88905abd4589a1a25fbb3e1a7cb
Rui Oliveira <ruimail24@gmail.com>
2023-08-22 13:04:39 +0000

    Use the identity components on the MainLayout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,38 +1,8 @@
 <template>
   <q-layout view="hhh LpR fFf">
-    <q-header bordered class="bg-primary text-white">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="left_drawer.toggle()"
-        />
+    <TopHeader @hamburger-clicked="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer show-if-above v-model="left_drawer.open" side="left" bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <LeftDrawer v-model="leftDrawerOpen" />
 
     <q-page-container>
       <router-view />
@@ -41,56 +11,20 @@
 </template>
 
 <script setup lang="ts">
-import EssentialLink, {
-  EssentialLinkProps,
-} from 'components/EssentialLink.vue';
+import { ref } from 'vue';
+
+import TopHeader from 'components/identity/TopHeader.vue';
+import LeftDrawer from 'components/identity/LeftDrawer.vue';
 
 import { useLeftDrawerStore } from 'stores/left-drawer';
 
 const left_drawer = useLeftDrawerStore();
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
+const leftDrawerOpen = ref(true);
+
+function toggleLeftDrawer() {
+  // TODO(ruilvo, 2023-08-22): Remove useless variables.
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+  left_drawer.open = leftDrawerOpen.value;
+}
 </script>
