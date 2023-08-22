9e753f40d4935171808c666e86862b12ee7bf4ab
Rui Oliveira <ruimail24@gmail.com>
2023-08-22 13:07:25 +0000

    Fix axios object creation

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -8,13 +8,11 @@ declare module '@vue/runtime-core' {
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+axios.defaults.xsrfCookieName = 'csrftoken';
+
+const api = axios.create();
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
