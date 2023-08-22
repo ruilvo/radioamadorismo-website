2ae8f04ec91c9770663cc49a4400a8f4d05713bb
Rui Oliveira <ruimail24@gmail.com>
2023-08-22 19:31:56 +0000

    Add repeaters page to the sidebar

--- frontend/src/components/identity/left_drawer/PagesItem.vue
+++ frontend/src/components/identity/left_drawer/PagesItem.vue
@@ -1,4 +1,10 @@
 <template>
+  <q-item key="repeaters-item" clickable :to="{ name: 'repeaters' }" exact>
+    <q-item-section avatar>
+      <q-icon name="cell_tower" />
+    </q-item-section>
+    <q-item-section>Repetidores</q-item-section>
+  </q-item>
   <q-item
     key="aprs-passcode-item"
     clickable
