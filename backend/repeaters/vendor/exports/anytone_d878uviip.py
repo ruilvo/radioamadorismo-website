613e503a49758fd5a610dc4398bbba397b178c06
Rui Oliveira <ruimail24@gmail.com>
2023-08-22 08:24:43 +0000

    Introduce a zip export for the Anytone codeplug

--- backend/repeaters/vendor/exports/anytone_d878uviip.py
+++ backend/repeaters/vendor/exports/anytone_d878uviip.py
@@ -6,6 +6,7 @@ On the Anytone D878UVII+, names are limited to 16 chars.
 
 import io
 import csv
+import zipfile
 
 
 from django.db.models import Q
@@ -695,3 +696,28 @@ class ZoneAnytoneUVIIPlusSerializer:  # pylint: disable=too-few-public-methods
             )
 
         return sio
+
+
+def codeplug_zip() -> io.BytesIO:
+    """
+    Makes a .zip file with the necessary csv files.
+    """
+    tgs_serializer = DimDmrTgAnytoneUVIIPlusSerializer()
+    rx_group_call_list_serializer = ReceiveGroupsAnytoneUVIIPlusSerializer(
+        tgs_serializer
+    )
+    channel_serializer = ChannelAnytoneUVIIPlusSerializer()
+    zones_serializer = ZoneAnytoneUVIIPlusSerializer(channel_serializer)
+
+    file_io = io.BytesIO()
+    with zipfile.ZipFile(file_io, "w") as zip_file:
+        for fname, fdata in (
+            ("TalkGroups.csv", tgs_serializer),
+            ("ReceiveGroupCallList.csv", rx_group_call_list_serializer),
+            ("Channel.csv", channel_serializer),
+            ("Zone.csv", zones_serializer),
+        ):
+            with zip_file.open(fname, "w") as csv_file:
+                csv_file.write(fdata.generate_csv().getvalue().encode("utf-8"))
+
+    return file_io
