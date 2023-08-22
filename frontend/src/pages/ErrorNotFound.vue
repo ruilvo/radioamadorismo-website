ce97fee1a7e2d9a70a8164d2f0702ddb63784e48
Rui Oliveira <ruimail24@gmail.com>
2023-08-22 13:10:29 +0000

    Translate 404 page

--- frontend/src/pages/ErrorNotFound.vue
+++ frontend/src/pages/ErrorNotFound.vue
@@ -5,19 +5,17 @@
     <div>
       <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <div class="text-h2" style="opacity: 0.4">Oops. Nada aqui...</div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
         text-color="blue"
         unelevated
-        to="/"
+        :to="{ name: 'home' }"
         label="Go Home"
         no-caps
       />
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
