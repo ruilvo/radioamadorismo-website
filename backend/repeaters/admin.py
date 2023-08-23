e108df573a55ba88f2967f5cab0b75741393d8d2
Rui Oliveira <ruimail24@gmail.com>
2023-08-23 17:39:46 +0000

    Add also admin panels to other dimensions

--- backend/repeaters/admin.py
+++ backend/repeaters/admin.py
@@ -16,14 +16,107 @@ from repeaters.models import (
     FactRepeater,
 )
 
-admin.site.register(DimRf)
+
+class DimRfAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the DimRf model.
+    """
+
+    list_display = (
+        "id",
+        "channel",
+        "tx_mhz",
+        "rx_mhz",
+        "shift_mhz",
+        "band",
+        "mode",
+    )
+    search_fields = (
+        "id",
+        "channel",
+        "tx_mhz",
+        "rx_mhz",
+        "shift_mhz",
+        "band",
+        "mode",
+    )
+    ordering = (
+        "id",
+        "channel",
+        "tx_mhz",
+        "rx_mhz",
+        "shift_mhz",
+        "band",
+        "mode",
+    )
+
+
+admin.site.register(DimRf, DimRfAdmin)
+
 admin.site.register(DimFm)
 admin.site.register(DimDStar)
 admin.site.register(DimFusion)
 admin.site.register(DimDmrTg)
 admin.site.register(DimDmr)
-admin.site.register(DimHolder)
-admin.site.register(DimLocation)
+
+
+class DimHolderAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the DimHolder model.
+    """
+
+    list_display = (
+        "id",
+        "abrv",
+        "name",
+    )
+    search_fields = (
+        "id",
+        "abrv",
+        "name",
+    )
+    ordering = (
+        "id",
+        "abrv",
+        "name",
+    )
+
+
+admin.site.register(DimHolder, DimHolderAdmin)
+
+
+class DimLocationAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the DimLocation model.
+    """
+
+    list_display = (
+        "id",
+        "place",
+        "region",
+        "qth_loc",
+        "latitude",
+        "longitude",
+    )
+    search_fields = (
+        "id",
+        "place",
+        "region",
+        "qth_loc",
+        "latitude",
+        "longitude",
+    )
+    ordering = (
+        "id",
+        "place",
+        "region",
+        "qth_loc",
+        "latitude",
+        "longitude",
+    )
+
+
+admin.site.register(DimLocation, DimLocationAdmin)
 
 
 class FactRepeaterAdmin(admin.ModelAdmin):
