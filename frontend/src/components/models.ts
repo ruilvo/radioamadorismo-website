352927453902a4c32daec3b348d4b4e3e43652fa
Rui Oliveira <ruimail24@gmail.com>
2023-08-26 15:05:59 +0000

    Add TS models for the repeaters API

--- frontend/src/components/models.ts
+++ frontend/src/components/models.ts
@@ -1,8 +1,80 @@
-export interface Todo {
+export interface DimRf {
   id: number;
-  content: string;
+  tx_mhz: string; // Comes in as a string, but is a number
+  rx_mhz: string; // Comes in as a string, but is a number
+  channel: null | string;
+  mode: string; // TODO: enum
+  shift_mhz: string; // Comes in as a string, but is a number
 }
 
-export interface Meta {
-  totalCount: number;
+export interface DimFm {
+  id: number;
+  modulation: string;
+  tone: null | string; // Comes in as a string, but is a number
+  bandwidth: string; // TODO: enum
+}
+
+export interface DimDStar {
+  id: number;
+  gateway: string;
+  reflector: string;
+}
+
+export interface DimFusion {
+  id: number;
+  wiresx: string;
+  room_id: string;
+}
+
+export interface DimDmrTg {
+  id: number;
+  name: string;
+  call_mode: string; // TODO: enum
+}
+
+export interface DimDmr {
+  id: number;
+  tg: DimDmrTg;
+  ts1_default_tg: DimDmrTg;
+  ts2_default_tg: DimDmrTg;
+  ts1_alternative_tgs: Array<DimDmrTg>;
+  ts2_alternative_tgs: Array<DimDmrTg>;
+  color_code: number;
+  ts_configuration: string;
+}
+
+export interface Association {
+  id: number;
+  abrv: string;
+  name: string;
+  email: string;
+  website: string;
+  notes: string;
+}
+
+export interface DimLocation {
+  id: number;
+  latitude: null | string; // Comes in as a string, but is a number
+  longitude: null | string; // Comes in as a string, but is a number
+  region: string; // TODO: enum
+  place: string;
+  qth_loc: string;
+}
+
+export interface Repeater {
+  id: number;
+  info_rf: null | DimRf;
+  info_fm: null | DimFm;
+  info_dstar: null | DimDStar;
+  info_fusion: null | DimFusion;
+  info_dmr: null | DimDmr;
+  info_holder: null | Association;
+  info_location: null | DimLocation;
+  callsign: string;
+  notes: string;
+  pwr_w: null | number;
+  status: string; // TODO: enum
+  sysop: string;
+  modes: Array<string>; // TODO: enum
+  info_tetra: null | number;
 }
