01a66dc5c71dd01d95419993ab02e641c0aecc57
Rui Oliveira <ruimail24@gmail.com>
2023-08-27 11:00:39 +0000

    Add ordering of fields to the backend

--- backend/repeaters/api/views.py
+++ backend/repeaters/api/views.py
@@ -76,6 +76,41 @@ class FactRepeaterViewSet(  # pylint: disable=too-many-ancestors
 
     filterset_class = FactRepeaterFilter
 
+    ordering_fields = (
+        "callsign",
+        "pwr_w",
+        "status",
+        "info_rf__tx_mhz",
+        "info_rf__rx_mhz",
+        "info_rf__channel",
+        "info_rf__band",
+        "info_rf__mode",
+        "info_rf__shift_mhz",
+        "info_fm__modulation",
+        "info_fm__tone",
+        "info_fm__bandwidth",
+        "info_dstar__gateway",
+        "info_dstar__reflector",
+        "info_fusion__wiresx",
+        "info_fusion__room_id",
+        "info_dmr__tg__id",
+        "info_dmr__tg__name",
+        "info_dmr__tg__call_mode",
+        "info_dmr__color_code",
+        "info_tetra__mcc",
+        "info_tetra__mnc",
+        "info_location__latitude",
+        "info_location__longitude",
+        "info_location__region",
+        "info_location__place",
+        "info_location__qth_loc",
+    )
+
+    ordering = (  # Default ordering
+        "info_location__region",
+        "-info_location__latitude",
+    )
+
 
 class DimRfViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ancestors
     """
@@ -85,6 +120,14 @@ class DimRfViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ance
     queryset = DimRf.objects.all()
     serializer_class = DimRfSerializer
     filterset_class = DimRfFilter
+    ordering_fields = (
+        "tx_mhz",
+        "rx_mhz",
+        "channel",
+        "band",
+        "mode",
+        "shift_mhz",
+    )
 
 
 class DimFmViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ancestors
@@ -94,6 +137,11 @@ class DimFmViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ance
 
     queryset = DimFm.objects.all()
     serializer_class = DimFmSerializer
+    ordering_fields = (
+        "modulation",
+        "tone",
+        "bandwidth",
+    )
 
 
 class DimDstarViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ancestors
@@ -103,6 +151,10 @@ class DimDstarViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-a
 
     queryset = DimDstar.objects.all()
     serializer_class = DimDstarSerializer
+    ordering_fields = (
+        "gateway",
+        "reflector",
+    )
 
 
 class DimFusionViewSet(  # pylint: disable=too-many-ancestors
@@ -114,6 +166,10 @@ class DimFusionViewSet(  # pylint: disable=too-many-ancestors
 
     queryset = DimFusion.objects.all()
     serializer_class = DimFusionSerializer
+    ordering_fields = (
+        "wiresx",
+        "room_id",
+    )
 
 
 class DimDmrTgViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ancestors
@@ -123,6 +179,11 @@ class DimDmrTgViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-a
 
     queryset = DimDmrTg.objects.all()
     serializer_class = DimDmrTgSerializer
+    ordering_fields = (
+        "id",
+        "name",
+        "call_mode",
+    )
 
 
 class DimDmrViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ancestors
@@ -132,6 +193,12 @@ class DimDmrViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-anc
 
     queryset = DimDmr.objects.all()
     serializer_class = DimDmrSerializer
+    ordering_fields = (
+        "tg__id",
+        "tg__name",
+        "tg__call_mode",
+        "color_code",
+    )
 
 
 class DimTetraViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-ancestors
@@ -141,6 +208,10 @@ class DimTetraViewSet(NestedWritableModelViewSet):  # pylint: disable=too-many-a
 
     queryset = DimTetra.objects.all()
     serializer_class = DimTetraSerializer
+    ordering_fields = (
+        "mcc",
+        "mnc",
+    )
 
 
 class DimLocationViewSet(  # pylint: disable=too-many-ancestors
@@ -153,3 +224,10 @@ class DimLocationViewSet(  # pylint: disable=too-many-ancestors
     queryset = DimLocation.objects.all()
     serializer_class = DimLocationSerializer
     filterset_class = DimLocationFilter
+    ordering_fields = (
+        "latitude",
+        "longitude",
+        "region",
+        "place",
+        "qth_loc",
+    )
