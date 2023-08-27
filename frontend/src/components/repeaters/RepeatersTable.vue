e51d9117c8bc54bf01b41eca03bbefe20e00ed3b
Rui Oliveira <ruimail24@gmail.com>
2023-08-27 10:16:18 +0000

    Implement server-side pagination

--- frontend/src/components/repeaters/RepeatersTable.vue
+++ frontend/src/components/repeaters/RepeatersTable.vue
@@ -3,6 +3,9 @@
     <q-table
       :rows="repeaters"
       :columns="columns"
+      :loading="loading"
+      v-model:pagination="pagination"
+      @request="onRequest"
       no-data-label="Sem dados para apresentar"
       no-results-label="Sem resultados para apresentar"
       row-key="id"
@@ -43,15 +46,153 @@ import { api } from 'boot/axios';
 
 import { FactRepeater } from 'src/components/models';
 
+const loading = ref(false);
+
+interface Pagination {
+  page: number;
+  rowsPerPage: number;
+  rowsNumber: number;
+  sortBy: string | null;
+  descending: boolean;
+}
+
+const pagination: Ref<Pagination> = ref({
+  page: 1,
+  rowsPerPage: 10,
+  rowsNumber: 10,
+  sortBy: null,
+  descending: false,
+});
+
 const repeaters: Ref<Array<FactRepeater>> = ref([]);
 
-onMounted(async () => {
+async function requestRepeaters(limit: number, offset: number): Promise<void> {
+  loading.value = true;
   try {
-    const response = await api.get('/api/v1/repeaters/fact-repeater/?limit=10');
+    const response = await api.get(
+      `/api/v1/repeaters/fact-repeater/?limit=${limit}&offset=${offset}`,
+    );
     repeaters.value = response.data.results;
+    pagination.value.rowsNumber = response.data.count;
   } catch (error) {
     console.error(error);
   }
+  loading.value = false;
+}
+
+function sortRepeaters(rowKey: string, descending: boolean) {
+  // TODO(ruilvo, 2023-08-27): In the future, this should be done in the backend
+  switch (rowKey) {
+    case 'callsign':
+      repeaters.value.sort((a, b) => {
+        return descending
+          ? b.callsign.localeCompare(a.callsign)
+          : a.callsign.localeCompare(b.callsign);
+      });
+      break;
+    case 'info_location__place':
+      repeaters.value.sort((a, b) => {
+        if (a.info_location == null || b.info_location == null) {
+          return 0;
+        }
+        return descending
+          ? b.info_location.place.localeCompare(a.info_location.place)
+          : a.info_location.place.localeCompare(b.info_location.place);
+      });
+      break;
+    case 'info_location__qth_loc':
+      repeaters.value.sort((a, b) => {
+        if (a.info_location == null || b.info_location == null) {
+          return 0;
+        }
+        return descending
+          ? b.info_location.qth_loc.localeCompare(a.info_location.qth_loc)
+          : a.info_location.qth_loc.localeCompare(b.info_location.qth_loc);
+      });
+      break;
+    case 'info_holder__abrv':
+      repeaters.value.sort((a, b) => {
+        if (a.info_holder == null || b.info_holder == null) {
+          return 0;
+        }
+        return descending
+          ? b.info_holder.abrv.localeCompare(a.info_holder.abrv)
+          : a.info_holder.abrv.localeCompare(b.info_holder.abrv);
+      });
+      break;
+    case 'info_holder__name':
+      repeaters.value.sort((a, b) => {
+        if (a.info_holder == null || b.info_holder == null) {
+          return 0;
+        }
+        return descending
+          ? b.info_holder.name.localeCompare(a.info_holder.name)
+          : a.info_holder.name.localeCompare(b.info_holder.name);
+      });
+      break;
+    case 'info_rf__tx_mhz':
+      repeaters.value.sort((a, b) => {
+        if (a.info_rf == null || b.info_rf == null) {
+          return 0;
+        }
+        return descending
+          ? Number.parseFloat(b.info_rf.tx_mhz) -
+              Number.parseFloat(a.info_rf.tx_mhz)
+          : Number.parseFloat(a.info_rf.tx_mhz) -
+              Number.parseFloat(b.info_rf.tx_mhz);
+      });
+      break;
+    case 'info_rf__rx_mhz':
+      repeaters.value.sort((a, b) => {
+        if (a.info_rf == null || b.info_rf == null) {
+          return 0;
+        }
+        return descending
+          ? Number.parseFloat(b.info_rf.rx_mhz) -
+              Number.parseFloat(a.info_rf.rx_mhz)
+          : Number.parseFloat(a.info_rf.rx_mhz) -
+              Number.parseFloat(b.info_rf.rx_mhz);
+      });
+      break;
+    case 'modes':
+      repeaters.value.sort((a, b) => {
+        return descending
+          ? format_modes_field(b.modes).localeCompare(
+              format_modes_field(a.modes),
+            )
+          : format_modes_field(a.modes).localeCompare(
+              format_modes_field(b.modes),
+            );
+      });
+      break;
+    default:
+      break;
+  }
+}
+
+function onRequest(props: { pagination: typeof pagination.value }) {
+  const { page, rowsPerPage, sortBy, descending } = props.pagination;
+  console.log(sortBy, descending);
+  var limit = rowsPerPage;
+  if (limit === 0) {
+    limit = pagination.value.rowsNumber;
+  }
+  const offset = (page - 1) * limit;
+  requestRepeaters(limit, offset).then(() => {
+    if (sortBy != null) {
+      sortRepeaters(sortBy, descending);
+    }
+  });
+  pagination.value.page = page;
+  pagination.value.rowsPerPage = rowsPerPage;
+  pagination.value.sortBy = sortBy;
+  pagination.value.descending = descending;
+}
+
+onMounted(() => {
+  const limit = pagination.value.rowsPerPage;
+  const offset = (pagination.value.page - 1) * limit;
+  requestRepeaters(limit, offset);
 });
 
 function format_rf_field(field: string): string {
