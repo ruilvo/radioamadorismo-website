d444cea71842b0667cf2ef940da6e92250c4c80b
Rui Oliveira <ruimail24@gmail.com>
2023-11-24 12:09:55 +0000

    Add view for the anytone export

--- backend/repeaters/urls.py
+++ backend/repeaters/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 
 from repeaters.views import (
     chirp_csv_view,
+    anytone_d878uviip_view,
 )
 
 urlpatterns = [
@@ -17,6 +18,11 @@ urlpatterns = [
                     "chirp/",
                     chirp_csv_view,
                     name="chirp_csv_view",
+                ),
+                path(
+                    "anytone_d878uviip/",
+                    anytone_d878uviip_view,
+                    name="anytone_d878uviip_view",
                 ),
             ]
         ),
--- backend/repeaters/views.py
+++ backend/repeaters/views.py
@@ -8,8 +8,16 @@ from django.http import HttpRequest
 from portal.responses import generate_csv_response, generate_zip_response
 
 from repeaters.exports.chirp import chirp_csv
+from repeaters.exports.anytone_d878uviip import RepeatersSerializer
 
 
 @require_safe
 def chirp_csv_view(_: HttpRequest):
     return generate_csv_response("chirp.csv", chirp_csv())
+
+
+@require_safe
+def anytone_d878uviip_view(_: HttpRequest):
+    return generate_zip_response(
+        "anytone_d878uviip.zip", RepeatersSerializer().codeplug
+    )
